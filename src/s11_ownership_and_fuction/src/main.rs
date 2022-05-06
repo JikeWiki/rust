4a7208259de125a7855c3e5ca09bbd4c294ee213
panhongyuan <i@phy.xyz>
2022-05-06 20:48:06 +0800

    12

--- src/s11_ownership_and_fuction/src/main.rs
+++ src/s11_ownership_and_fuction/src/main.rs
@@ -1,15 +1,8 @@
 fn main() {
-    let s1 = String::from("hello");
-
-    // 在main函数里将s1的所有权移动给calculate_length函数，在 calculate_length 再将所有权移交回来
-    let (s1, len) = calculate_length(s1);
-
-    println!("The length of '{}' is {}.", s1, len);
+    let r = dangling();
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len();
-
-    // calculate_length 函数再将s的所有权移动给 调用它的函数
-    (s, length)
+fn dangling() -> &String {
+    let s = String::from("hello");
+    &s
 }
